f82f7f905de6ca0ae4a0368b267bf07aa84ad137
Recyclingbottle <hoholeo382@gmail.com>
2022-11-28 14:50:21 +0900

    List, Tree View

--- WPFList/MainWindow.xaml.cs
+++ WPFList/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,36 @@ namespace WPFList
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //ListViewItem item = (ListViewItem)FList.SelectedItem;
+            //TextBlock t = (TextBlock)((StackPanel)item.Content).Children[1];
+
+            //MessageBox.Show(t.Text);
+            //try
+            //{
+            //    BitmapImage b = new BitmapImage(new Uri("pack://application:,,,/WPFList;" + $"component/Images/{t.Text}.jpg"));
+            //    F_image.Source = b;
+            //} catch(IOException err) {
+            //    MessageBox.Show(err.Message);
+            //    F_image.Source = null;
+            //}
+            //ctrl+k+c 주식화,  ctrl+k+u 풀기
+
+            //BitmapImage b = new BitmapImage(new Uri($"Images/{t.Text}.jpg", UriKind.RelativeOrAbsolute));
+            //F_image.Source = b;
+
+
+
+        }
+
+        private void Onselected(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            TreeViewItem item = (TreeViewItem)FTree.SelectedItem;
+            BitmapImage b = new BitmapImage(new Uri($"Images/{item.Header}.jpg", UriKind.RelativeOrAbsolute));
+            F_image.Source = b;
         }
     }
 }
